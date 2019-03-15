22806a337519bf9a7e9749c8cb0445f7a67febc7
Theale <tgghastelow@gmail.com>
2019-03-16 06:42:24 +1000

    Updates

---
+++ precache-manifest.6933bb43758ae59c9cf92541b8389144.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fce8213f5edc7382efa7db3c1a4e9602",
+    "url": "/static/media/robofriends.fce8213f.jpg"
+  },
+  {
+    "revision": "297700ea0f14c7bbaebeb72e93fa9c98",
+    "url": "/static/media/pokemon-pairs.297700ea.jpg"
+  },
+  {
+    "revision": "bed37abf75b0e7797234c93ac5e57b91",
+    "url": "/static/media/face-recognition.bed37abf.jpg"
+  },
+  {
+    "revision": "fcc28a11e891da44d17a29ac9011022c",
+    "url": "/static/media/CMoQ.fcc28a11.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8b9f385a97339aea24f0",
+    "url": "/static/js/main.507cc7f9.chunk.js"
+  },
+  {
+    "revision": "ccb4977e4d1e0c539eec",
+    "url": "/static/js/2.783c090b.chunk.js"
+  },
+  {
+    "revision": "8b9f385a97339aea24f0",
+    "url": "/static/css/main.c935db51.chunk.css"
+  },
+  {
+    "revision": "319fe2a6cd275a8d0374ba9554b612e3",
+    "url": "/index.html"
+  }
+];
