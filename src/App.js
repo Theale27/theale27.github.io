17983bfb0f35a3223a8fc5c28ff94c10d9224547
Theale <tgghastelow@gmail.com>
2019-03-19 02:30:41 +1000

    Added redirect handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import Navigation from "./components/navigation";
 import Footer from "./components/footer";
 import AboutMe from "./components/about-me";
@@ -23,15 +28,30 @@ class App extends Component {
       }
     ];
 
+    const NoMatch = ({ location }) => {
+      return (
+        <div>
+          <h3>
+            No match for <code>{location.pathname}</code>
+          </h3>
+        </div>
+      );
+    };
+
     return (
       <Router>
         <div className="App">
           <Navigation navLinks={navLinks} />
           <Switch>
+            <Route exact path={navLinks[0].link} component={AboutMe} />
             <Route path={navLinks[1].link} component={Skills} />
             <Route path={navLinks[2].link} component={Projects} />
-            {/* Switch finds the first match which is why home ('/') is last */}
-            <Route path={navLinks[0].link} component={AboutMe} />
+
+            <Redirect exact from="/" to="/" />
+            <Redirect from="/skills" to="/skills" />
+            <Redirect from="projects" to="projects" />
+
+            <Route component={NoMatch} />
           </Switch>
           <Footer />
         </div>
