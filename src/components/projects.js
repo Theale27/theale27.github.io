f455a29b496c8dbac2f9d83021e5c2544f7c6d89
Theale <tgghastelow@gmail.com>
2019-07-13 22:38:10 +1000

    Added the Video Sharing Site

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,6 +11,98 @@ class Projects extends Component {
       <main>
         <section id="projects-container">
           <div className="project">
+            <img src={img_cmoq} alt="Project" />
+            <div className="p-info">
+              <h3>Chinese Museum of Queensland (CMoQ)</h3>
+              <p>
+                This website was an industry project with the University of the
+                Sunshine Coast and was designed and developed as part of a 3
+                person team using HTML, CSS, JavaScript, Bootstrap 3, and the
+                Google Maps API.
+                <br />I received a high distinction with 92%.
+              </p>
+              <div className="btns">
+                <a href="http://cmoq.com.au/" className="live-btn">
+                  Live
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="project">
+            <div className="p-info">
+              <h3>Websites made during University</h3>
+              <hr />
+              <p>
+                <strong>Novacare Website</strong> - This website was made for a
+                University assignment in 2014 and uses HTML, CSS,
+                JavaScript/jQuery, and the image plugin lightbox.
+              </p>
+              <div className="btns">
+                <a
+                  href="https://theale27.github.io/Novacare-Website/index.html"
+                  className="live-btn"
+                >
+                  Live
+                </a>
+                <a
+                  href="https://github.com/theale27/Novacare-Website"
+                  className="github-btn"
+                >
+                  GitHub
+                </a>
+              </div>
+              <br />
+              <p>
+                <strong>Band Website</strong> - This website was made for a
+                University assignment in 2016 which was to advertise a band's
+                tour by providing a responsive webpage with an Australia theme.
+                It uses HTML, CSS, JavaScript/jQuery, and PHP.
+              </p>
+              <div className="btns">
+                <a
+                  href="https://band-website-2016.herokuapp.com"
+                  className="live-btn"
+                >
+                  Live
+                </a>
+                <a
+                  href="https://github.com/theale27/Band-Website"
+                  className="github-btn"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="project">
+            <div className="p-info">
+              <h3>Other Sites</h3>
+              <hr />
+              <p>
+                <strong>Video Sharing Site</strong> - This is a YouTube-like
+                site that hosts videos and has social media aspects like
+                comments, and profiles. It is made with React, JavaScript, HTML,
+                Sass, and uses React Router for navigation.
+              </p>
+              <div className="btns">
+                <a
+                  href="https://video-sharing-site.herokuapp.com/"
+                  className="live-btn"
+                >
+                  Live
+                </a>
+                <a
+                  href="https://github.com/theale27/Video-Sharing-Site"
+                  className="github-btn"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="project">
             <img src={img_faceRecognition} alt="Face Recognition App" />
             <div className="p-info">
               <h3>Face Recognition App</h3>
@@ -81,24 +173,6 @@ class Projects extends Component {
             </div>
           </div>
           <div className="project">
-            <img src={img_cmoq} alt="Project" />
-            <div className="p-info">
-              <h3>Chinese Museum of Queensland (CMoQ)</h3>
-              <p>
-                This website was an industry project with the University of the
-                Sunshine Coast and was designed and developed as part of a 3
-                person team using HTML, CSS, JavaScript, Bootstrap 3, and the
-                Google Maps API.
-                <br />I received a high distinction with 92%.
-              </p>
-              <div className="btns">
-                <a href="http://cmoq.com.au/" className="live-btn">
-                  Live
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="project">
             <img src={img_pokemonPairs} alt="Pokemon Pairs" />
             <div className="p-info">
               <h3>Pokemon Pairs</h3>
@@ -121,52 +195,6 @@ class Projects extends Component {
                 </a>
                 <a
                   href="https://github.com/theale27/pokemon-pairs"
-                  className="github-btn"
-                >
-                  GitHub
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="project">
-            <div className="p-info">
-              <h3>Websites made during University</h3>
-              <hr />
-              <p>
-                Novacare Website - This website was made for a University
-                assignment in 2014 and uses HTML, CSS, JavaScript/jQuery, and
-                the image plugin lightbox.
-              </p>
-              <div className="btns">
-                <a
-                  href="https://theale27.github.io/Novacare-Website/index.html"
-                  className="live-btn"
-                >
-                  Live
-                </a>
-                <a
-                  href="https://github.com/theale27/Novacare-Website"
-                  className="github-btn"
-                >
-                  GitHub
-                </a>
-              </div>
-              <br />
-              <p>
-                Band Website - This website was made for a University assignment
-                in 2016 which was to advertise a band's tour by providing a
-                responsive webpage with an Australia theme. It uses HTML, CSS,
-                JavaScript/jQuery, and PHP.
-              </p>
-              <div className="btns">
-                <a
-                  href="https://band-website-2016.herokuapp.com"
-                  className="live-btn"
-                >
-                  Live
-                </a>
-                <a
-                  href="https://github.com/theale27/Band-Website"
                   className="github-btn"
                 >
                   GitHub
